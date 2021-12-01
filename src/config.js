1d3676778f00eb6b2b9c06a73580b3803e07e1bc
ger88555 <germanadolf@protonmail.com>
2021-12-01 08:43:31 -0700

    Añadida configuración del proyecto

---
+++ src/config.js
@@ -0,0 +1,8 @@
+/**
+ * Configuración de la API
+ */
+module.exports = {
+    HOST: process.env.HOST || 'localhost',
+    PORT: process.env.PORT || 8080,
+    CUENTA_BANCO: process.env.CUENTA_BANCO
+}
